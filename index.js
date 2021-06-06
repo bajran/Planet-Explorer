dfb0025e65125ea52b10fef696b3bc82793e85f3
youraksh1594540052451 <youraksh@gmail.com>
2021-06-06 15:35:56 +0530

    Retrieve Habitual Planet Name

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const parse = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+    return planet["koi_disposition"] === "CONFIRMED"
+        && planet["koi_insol"] > 0.36
+        && planet["koi_insol"] < 1.11
+        && planet["koi_prad"] < 1.6
+}
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
@@ -9,12 +16,15 @@ fs.createReadStream('kepler_data.csv')
         columns: true
     }))
     .on('data', (chunk) => {
-        results.push(chunk)
+        if (isHabitablePlanet(chunk)) {
+            habitablePlanets.push(chunk)
+        }
     })
     .on('error', (err) => {
         console.log(`Error Occured ${err}`)
     })
     .on('end', () => {
-        console.log(results);
+        console.log(habitablePlanets.map((planet) => planet["kepler_name"]))
+        console.log(`${habitablePlanets.length} Planet found`);
         console.log('done');
     })
